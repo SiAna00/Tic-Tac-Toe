e4e008396401def2f707808f457fc59ee7a4cc1e
anasi <asimicev@gmail.com>
2024-02-14 15:30:02 +0100

    add game logic

--- main.py
+++ main.py
@@ -5,32 +5,41 @@ board = " 1 | 2 | 3 \n-----------\n 4 | 5 | 6 \n-----------\n 7 | 8 | 9 \n"
 print(board)
 
 players = {
-    "player_a" : "X",
-    "player_b" : "O"
+    "Player A" : {
+        "sign" : "X",
+         "combination" : []
+    },
+    "Player B" : {
+        "sign" : "O",
+        "combination" : []
+    }
 }
 
-for player in players:
-    player_input = input("Choose a number: ")
+winning_combinations = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 5, 9], [3, 5, 7], [1, 4, 7], [2, 5, 8], [3, 6, 9]]
 
-    for character in board:
-        if character == player_input:
-            board = board.replace(character, players[player])
+game_on = True
 
-    print(board)
+while game_on == True:
+    for player in players:
 
+        if player == "Player B" and len(players[player]["combination"]) == 4:
+            print("Game over. It's a draw.")
+            game_on = False
 
-""" player_a = input("Choose a number: ")
+        else:
+            player_input = input(f"{player}: Choose a number: ")
 
-for character in board:
-    if character == player_a:
-        board = board.replace(character, "X")
+            for character in board:
+                if character == player_input:
+                    board = board.replace(character, players[player]["sign"])
+                    players[player]["combination"].append(int(player_input))
 
-print(board)
+                    if len(players[player]["combination"]) >= 3:
+                        players[player]["combination"].sort()
 
-player_b = input("Choose a number: ")
+                        for item in winning_combinations:
+                            if set(item) <= set(players[player]["combination"]):
+                                print(f"Congratulations! {player} is the winner!")
+                                game_on = False
 
-for character in board:
-    if character == player_b:
-        board = board.replace(character, "O")
-
-print(board) """
+            print(board)
